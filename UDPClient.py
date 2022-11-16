68a38673095036ae59b8622196e6a3f47cfdf1dd
Anish Dhandore <anish.dhandore@gmail.com>
2022-11-15 21:54:34 -0800

    ready for submission

--- UDPClient.py
+++ UDPClient.py
@@ -1,12 +1,12 @@
 from socket import *
 import time
 import json
+import os
+import shutil
 
 serverName = 'localhost'
 serverPort = 18000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-b = b''
 
 def send_message(clientSocket, msg):
     if type(msg) == dict:
@@ -15,8 +15,6 @@ def send_message(clientSocket, msg):
 
     else:
         clientSocket.sendto(str(msg).encode('base64','strict'),(serverName, serverPort))
-        # modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        # print(modifiedMessage.decode())
 
 def start():
     print("Choose one of the options from the menu:")
@@ -26,7 +24,10 @@ def start():
     message = input("Enter your selection: ")
 
     b = b''
+    c = b''
+    e = b''
     if message == "1":
+        filename = input("Enter the location of your file: ")
         print("1. Use HTTP version 1.0 (non-persistent)")
         print("2. Use HTTP version 1.1 (persistent)")
 
@@ -36,10 +37,6 @@ def start():
             http_ver = 1.0
         else:
             http_ver = 1.1
-
-        # tcp_syn_msg = f'''PAYLOAD LENGTH: {0}\nTCP_SYN_FLAG: {1}\nTCP_ACK_FLAG: {0}\n
-        # TCP_FIN_FLAG: {0}\nHTTP_GET_REQUEST: {1}\nHTTP_RESPONSE_STATUS_CODE: {0}\n
-        # HTTP_CLIENT_VERSION: {str(http_ver)}\nHTTP_REQUEST_PATH: {0}\nHTTP_INCLUDED_OBJECT_PATH: {0}'''
 
         tcp_syn_msg = {"PAYLOAD LENGTH": 0, 
         "TCP_SYN_FLAG": 1, 
@@ -56,7 +53,6 @@ def start():
         print("[Client] Sent a TCP SYN message to the server!!!")
 
         modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-        # print(modifiedMessage.decode())
 
         b += modifiedMessage
         d = json.loads(b.decode('utf-8'))
@@ -78,7 +74,6 @@ def start():
         print("[Client] Client and Server are in a TCP connection now!!!")
 
         # send a http get request
-        filename = input("What file are you looking for: ")
         http_get = {"PAYLOAD LENGTH": 0, 
         "TCP_SYN_FLAG": 0, 
         "TCP_ACK_FLAG": 0,
@@ -86,15 +81,79 @@ def start():
         "HTTP_GET_REQUEST": 1,
         "HTTP_RESPONSE_STATUS_CODE": 0,
         "HTTP_CLIENT_VERSION": http_ver,
-        "HTTP_REQUEST_PATH": "/attachments/"+filename+".html",
+        "HTTP_REQUEST_PATH": filename,
         "HTTP_INCLUDED_OBJECT_PATH": 0}
 
         send_message(clientSocket, http_get) 
 
+        # get the http response message now 
+        statusMessage, serverAddress = clientSocket.recvfrom(2048)
+        c += statusMessage
+        d = json.loads(c.decode('utf-8'))
+        
+        # if status code is 404, display page not found
+        if d["HTTP_RESPONSE_STATUS_CODE"] == 404:
+            print("[Client] Error 404...the page you are looking for is not found!!!")
+
+            # send a TCP FIN message to the server to close the connection 
+            tcp_fin_msg = {"PAYLOAD LENGTH": 0, 
+            "TCP_SYN_FLAG": 0, 
+            "TCP_ACK_FLAG": 0,
+            "TCP_FIN_FLAG": 1,
+            "HTTP_GET_REQUEST": 1,
+            "HTTP_RESPONSE_STATUS_CODE": 404,
+            "HTTP_CLIENT_VERSION": http_ver,
+            "HTTP_REQUEST_PATH": filename,
+            "HTTP_INCLUDED_OBJECT_PATH": 0}
+        
+            send_message(clientSocket, tcp_fin_msg)
+            print("[Client] Sent a TCP FIN message to the server...")
+
+            # receive a TCP FIN ACK message from the server 
+            tcpfinackMessage, serverAddress = clientSocket.recvfrom(2048)
+
+            # send a TCP ACK message again, and display menu again
+            tcp_ack_msg = {"PAYLOAD LENGTH": 0, 
+            "TCP_SYN_FLAG": 0, 
+            "TCP_ACK_FLAG": 1,
+            "TCP_FIN_FLAG": 0,
+            "HTTP_GET_REQUEST": 0,
+            "HTTP_RESPONSE_STATUS_CODE": 404,
+            "HTTP_CLIENT_VERSION": http_ver,
+            "HTTP_REQUEST_PATH": filename,
+            "HTTP_INCLUDED_OBJECT_PATH": 0}
+            send_message(clientSocket, tcp_ack_msg)
+
+            # show the first menu now
+            return
+
+        # if status code is 200, display page is found
+        else:
+            print("[Client] Success 200 OK...the page you are looking for is found!!!")
+
+            print("[Client] Contents of your file are as follows:")
+            print(d["CONTENTS"])
+
+            if d["HTTP_INCLUDED_OBJECT_PATH"] != "":
+                file = open(d["HTTP_INCLUDED_OBJECT_PATH"], mode='r')
+                file_contents = file.read()
+                file.close()
+
+                print("[Client] Contents of attached file:")
+                print(file_contents)
+
+            # storing the file in client machine 
+            original = os.getcwd() + "/" + d["HTTP_REQUEST_PATH"]
+            print(original)
+            target = "/Users/anishdhandore/Documents/CSUSM/Courses /7 Fall 2022/CS 436/FinalProject_Networking/Client"
+            shutil.copy(original, target)
     else:
         return
 
-start()
+if __name__ == '__main__':
+    while True:
+        start()
+
 
     
 
--- UDPServer.py
+++ UDPServer.py
@@ -13,31 +13,11 @@ print('[Server] The server is ready to receive')
 b = b''
 c = b''
 e = b''
+
 while True:
     firstmessage, clientAddress = serverSocket.recvfrom(2048)
-
-    # f = open('test.txt', mode='w')
-    # f.write(str(message))
-    # f.close()
-
-    # http version 1.0
-    # if str(message) == "b'1'":
-    #     print(f"Client [{clientAddress}] chose HTTP 1.0")
-
-    # # http version 1.1
-    # elif str(message) == "b'2'":
-    #     print(f"Client [{clientAddress}] chose HTTP 1.1")
-    
-    # else:
-    #     break
-
-    # modifiedMessage = f'''PAYLOAD LENGTH: {0}\nTCP_SYN_FLAG: {1}\nTCP_ACK_FLAG: {0}\n
-    #     TCP_FIN_FLAG: {0}\nHTTP_GET_REQUEST: {1}\nHTTP_RESPONSE_STATUS_CODE: {0}\n
-    #     HTTP_CLIENT_VERSION: {str(http_ver)}\nHTTP_REQUEST_PATH: {0}\nHTTP_INCLUDED_OBJECT_PATH: {0}'''
-
     b += firstmessage
     d = json.loads(b.decode('utf-8'))
-    print(d)
 
     http_ver = d["HTTP_CLIENT_VERSION"]
     if firstmessage:
@@ -66,25 +46,93 @@ while True:
 
         c += tcp_message
         d = json.loads(c.decode('utf-8'))
-        print(d)
 
         # get the http get request from the client 
         # check if the file exists in our directory
         http_message, clientAddress = serverSocket.recvfrom(2048)
         e += http_message
         d = json.loads(e.decode('utf-8'))
-        print(d)
 
-        link_to_file = e["HTTP_REQUEST_PATH"]
+        link_to_file = d["HTTP_REQUEST_PATH"]
         cwd = os.getcwd()
-        link_to_file_new = cwd + link_to_file
+        link_to_file_new = cwd + "/" + link_to_file
 
-        # file exists
-        if os.path.exists(link_to_file_new) == True:
-            # status code 200 OK
-            pass 
-        else:
+        # file does not exists
+        if os.path.exists(link_to_file_new) == False:
             # status code 404 page not found
-            pass
+            # server sends message to client with 404 error
 
+            error_message = {"PAYLOAD LENGTH": 0, 
+            "TCP_SYN_FLAG": 0, 
+            "TCP_ACK_FLAG": 0,
+            "TCP_FIN_FLAG": 0,
+            "HTTP_GET_REQUEST": 0,
+            "HTTP_RESPONSE_STATUS_CODE": 404,
+            "HTTP_CLIENT_VERSION": http_ver,
+            "HTTP_REQUEST_PATH": link_to_file,
+            "HTTP_INCLUDED_OBJECT_PATH": 0}
+
+            error_msg = json.dumps(error_message).encode('utf-8')
+            serverSocket.sendto(error_msg, clientAddress) 
+
+            # receive the TCP FIN message from the client
+            tcp_fin_message, clientAddress = serverSocket.recvfrom(2048)
+
+            # send the tcp fin ack message to the server
+            tcp_fin_ack = {"PAYLOAD LENGTH": 0, 
+            "TCP_SYN_FLAG": 0, 
+            "TCP_ACK_FLAG": 0,
+            "TCP_FIN_FLAG": 0,
+            "HTTP_GET_REQUEST": 0,
+            "HTTP_RESPONSE_STATUS_CODE": 404,
+            "HTTP_CLIENT_VERSION": http_ver,
+            "HTTP_REQUEST_PATH": link_to_file,
+            "HTTP_INCLUDED_OBJECT_PATH": 0}
+            tcp_fin_ack_msg = json.dumps(tcp_fin_ack).encode('utf-8')
+            serverSocket.sendto(tcp_fin_ack_msg, clientAddress) 
+
+            break
+
+        else:
+            # status code 200 OK
+            file = open(link_to_file_new, mode='r')
+            file_contents = file.read()
+            file.close()
+
+            # set the status code to 200
+            # include the link to the html file 
+            # include link to any object path
+            # include the contents of the file
+            # send the file to the client
+
+            object_path = ""
+
+            # first file has no objects
+            if link_to_file == "attachments/file1.html":
+                pass
+            # second file has link to third file
+            elif link_to_file == "attachments/file2.html":
+                object_path += "attachments/file3.html"
+            # third file has link to second file
+            elif link_to_file == "attachments/file1.html":
+                object_path += "attachments/file2.html"
+            else:
+                pass
+
+            final_message = {"PAYLOAD LENGTH": 0, 
+            "TCP_SYN_FLAG": 0, 
+            "TCP_ACK_FLAG": 0,
+            "TCP_FIN_FLAG": 0,
+            "HTTP_GET_REQUEST": 0,
+            "HTTP_RESPONSE_STATUS_CODE": 200,
+            "HTTP_CLIENT_VERSION": http_ver,
+            "HTTP_REQUEST_PATH": link_to_file,
+            "HTTP_INCLUDED_OBJECT_PATH": object_path,
+            "CONTENTS": file_contents}
+
+            final_msg = json.dumps(final_message).encode()
+            serverSocket.sendto(final_msg, clientAddress)
+            print("[Server] sent a message to the client with file information")
+            break
+            
     # serverSocket.close()
